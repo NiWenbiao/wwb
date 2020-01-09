aad8b32499f3789e43eb792b28692fa7bc6693ca
NiWenbiao <59614864+NiWenbiao@users.noreply.github.com>
2020-01-09 10:43:27 +0800

    Add files via upload

---
+++ ShoppingCart/java/com/cn/itcast/shoppingcart/ShopActivity.java
@@ -0,0 +1,110 @@
+package com.cn.itcast.shoppingcart;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class ShopActivity extends Activity implements View.OnClickListener{
+    private ListView mListView;
+    private ImageView add,car;
+    //商品名称与价格数据集合
+    private String[] titles = {"男装 仿羊羔绒运动长裤", "女装 仿羊羔绒摇粒绒拉链V领开衫(长袖)",
+            "男装 直筒工装长裤", "男装 HEATTECH保暖长裤", "男装 洗旧无褶直筒长裤",
+            "女装 HEATTECH摇粒绒两翻领T恤(长袖)", "男装 修身无褶长裤", "男装 防风弹力修身无褶长裤",
+            "男装 修身无褶长裤", "女装 罗纹棉质两翻领T恤(长袖)"};
+
+    private String[] prices = {"¥149.00", "¥149.00", "¥149.00", "¥149.00", "¥149.00",
+            "¥69.00", "¥149.00", "¥149.00", "¥149.00", "¥59.00"};
+
+    private int[] icons = {R.drawable.na,R.drawable.nb,R.drawable.nc,R.drawable.md,R.drawable.ne,
+            R.drawable.nf,R.drawable.ng,R.drawable.mh,R.drawable.ni,R.drawable.nj};
+
+    SQLiteHelper sqLiteHelper;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shop);
+        mListView = (ListView) findViewById(R.id.lv);//初始化ListView控件
+        add = (ImageView) findViewById(R.id.add);
+        car = (ImageView) findViewById(R.id.car);
+        MyBaseAdapter mAdapter = new MyBaseAdapter();
+        mListView.setAdapter(mAdapter);
+        initData();
+    }
+    protected void initData(){sqLiteHelper = new SQLiteHelper(this);}
+
+    @Override
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.car:
+                Intent intent = new Intent(ShopActivity.this,CarActivity.class);
+                startActivityForResult(intent,1);
+        }
+    }
+     class MyBaseAdapter extends BaseAdapter {
+        @Override
+        public int getCount() {
+            return titles.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return titles[position];
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder vh;
+            if (convertView == null){
+                convertView = View.inflate(ShopActivity.this,R.layout.shop_item,null);
+                vh = new ViewHolder();
+                vh.tvShopTitle = (TextView) convertView.findViewById(R.id.title);
+                vh.tvShopPrice = (TextView) convertView.findViewById(R.id.price);
+                vh.tvShopiv = (ImageView) convertView.findViewById(R.id.iv);
+                vh.tvShopadd = (ImageView) convertView.findViewById(R.id.add);
+                convertView.setTag(vh);
+                final ViewHolder finalVh = vh;
+                vh.tvShopadd.setOnClickListener(new View.OnClickListener(){
+                    @Override
+                    public void onClick(View v){
+                        String CarTitle = finalVh.tvShopTitle.getText().toString();
+                        String CarPrice = finalVh.tvShopPrice.getText().toString();
+                        if (sqLiteHelper.insertData(CarTitle,CarPrice)){
+                            Toast.makeText(ShopActivity.this,"添加成功",Toast.LENGTH_SHORT).show();
+                        }else{
+                            Toast.makeText(ShopActivity.this,"添加失败",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }else{
+                vh = (ViewHolder)convertView.getTag();
+            }
+            vh.tvShopTitle.setText(titles[position]);
+            vh.tvShopPrice.setText(prices[position]);
+            vh.tvShopiv.setBackgroundResource(icons[position]);
+            return convertView;
+        }
+        class ViewHolder {
+            TextView tvShopTitle;
+            TextView tvShopPrice;
+            ImageView tvShopadd;
+            ImageView tvShopiv;
+        }
+    }
+}
+
